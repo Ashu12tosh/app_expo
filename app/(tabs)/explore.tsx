12524ca483748ddf16ba8714ebfd6c3fec2f3a26
Ashu12tosh <ash8707672263@gmail.com>
2025-04-15 09:53:56 +0530

    updating code

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,9 +1,13 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function explore() {
   return (
     <View>
       <Text>Explore Page</Text>
+      <Link href={"/accountinfo"}>
+      <Text>Account information</Text>
+      </Link>
     </View>
   );
 }
